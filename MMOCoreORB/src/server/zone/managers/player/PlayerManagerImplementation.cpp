c7e41bd7541fa66597d793de3c8714dd95012f2f
orcsbane1369 <orcsbane1369@gmail.com>
2025-06-09 22:21:15 -0500

    Attempt #4 FRS XP

--- MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -2131,9 +2131,12 @@ void PlayerManagerImplementation::disseminateExperience(TangibleObject* destruct
 				// Jedi experience doesn't count towards combat experience, and is earned at 20% the rate of normal experience
 				if (xpType != "jedi_general")
 					combatXp += xpAmount;
-				else
-					
+				else {
 					xpAmount *= 0.8f;
+
+					if (attacker->hasSkill("force_title_jedi_rank_03"))
+						awardExperience(attackerCreo, "force_rank_xp", xpAmount, true, .01, true);
+				}
 
 				if (xpType == "dotDMG") { // Prevents XP generated from DoTs from applying to the equiped weapon, but still counts towards combat XP
 					continue;
@@ -2144,7 +2147,6 @@ void PlayerManagerImplementation::disseminateExperience(TangibleObject* destruct
 			}
 
 			awardExperience(attackerCreo, "combat_general", combatXp, true, 0.1f);
-			frsXp = awardExperience(attackerCreo, "force_rank_xp", frsXp, true, 0.01f);
 
 
 			//Check if the group leader is a squad leader
