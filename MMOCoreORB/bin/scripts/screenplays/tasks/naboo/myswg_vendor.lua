4a833973c42ad50d6bc918b35b6e173b4ea36366
orcsbane1369 <orcsbane1369@gmail.com>
2025-06-11 01:28:52 -0500

    Update myswg_vendor.lua

--- MMOCoreORB/bin/scripts/screenplays/tasks/naboo/myswg_vendor.lua
+++ MMOCoreORB/bin/scripts/screenplays/tasks/naboo/myswg_vendor.lua
@@ -67,7 +67,7 @@ function myswg_vendor:start()
 	
 	
 	--Brawler Trainer
-	local pWeaponsmith4 = spawnMobile("dantooine", "trainer_brawler", 1, -638, 3.0, 2515, 35, 0 )--mining
+	local pWeaponsmith4 = spawnMobile("dantooine", "trainer_brawler", 1, -636, 3.0, 2503, 35, 0 )--mining
     local pWeaponsmith4 = spawnMobile("dantooine", "trainer_brawler", 1, -4209, 3.0, -2359, 35, 0 )--imp
     local pWeaponsmith4 = spawnMobile("dantooine", "trainer_brawler", 1, 1564, 4.0, -6425, 35, 0 )--aggro
                 
