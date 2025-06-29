14d3d93a4445dcf30ed3503632a636d994a63bc9
orcsbane1369 <orcsbane1369@gmail.com>
2025-06-29 09:39:43 -0500

    Update global_imperial_outpost_commando_large_theater.lua

--- MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global/global_imperial_outpost_commando_large_theater.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global/global_imperial_outpost_commando_large_theater.lua
@@ -1,10 +1,8 @@
 global_imperial_outpost_commando_large_theater = Lair:new {
 	mobiles = {
-		{"storm_commando",5},
-		{"imperial_surface_marshal",5},
-		{"imperial_high_general",5,
-		{"imperial_general",5},
-		{"stormtrooper_novatrooper_elite_commander",5},
+		{"storm_commando",4},
+		{"imperial_surface_marshal",2},
+		{"stormtrooper_novatrooper_elite_commander",1},
 	},
 	spawnLimit = 25,
 	buildingsVeryEasy = {"object/building/poi/lok_imperial_large2.iff","object/building/poi/anywhere_imperial_base_large_1.iff"},
