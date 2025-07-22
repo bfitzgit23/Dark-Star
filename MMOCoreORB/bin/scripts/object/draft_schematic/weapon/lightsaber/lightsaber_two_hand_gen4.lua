ab64803cb1243849ef9b4e2b505a3d30d4c3f0a6
orcsbane1369 <orcsbane1369@gmail.com>
2025-07-22 17:47:56 -0500

    modifiying lightsabers

--- MMOCoreORB/bin/scripts/object/draft_schematic/weapon/lightsaber/lightsaber_two_hand_gen4.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/weapon/lightsaber/lightsaber_two_hand_gen4.lua
@@ -65,10 +65,10 @@ object_draft_schematic_weapon_lightsaber_lightsaber_two_hand_gen4 = object_draft
    customizationDefaults = {},
 
    ingredientTemplateNames = {"craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n"},
-   ingredientTitleNames = {"emitter_shroud", "primary_crystal", "activator", "handgrip", "focusing_crystals", "power_field_insulator", "energizers"},
-   ingredientSlotType = {0, 1, 0, 0, 1, 0, 0},
+   ingredientTitleNames = {"emitter_shroud", "primary_crystal", "activator", "handgrip", "focusing_crystals", "power_field_insulator", "energizers", "focusing_crystals", "focusing_crystals"},
+   ingredientSlotType = {0, 1, 0, 0, 1, 0, 0, 1, 1},
    resourceTypes = {"steel_duralloy", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff", "aluminum_titanium", "petrochem_inert_polymer", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff", "gas_inert_culsion", "copper_polysteel"},
-   resourceQuantities = {40, 1, 22, 28, 1, 28, 28},
+   resourceQuantities = {40, 1, 30, 46, 1, 58, 58},
    contribution = {100, 100, 100, 100, 100, 100, 100},
 
 
