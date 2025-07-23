d4d425d4df0fa148566b3082654a66122fb09b1d
orcsbane1369 <orcsbane1369@gmail.com>
2025-07-22 19:52:00 -0500

    added more compontents to sabers

--- MMOCoreORB/bin/scripts/object/draft_schematic/weapon/lightsaber/lightsaber_two_hand_gen4.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/weapon/lightsaber/lightsaber_two_hand_gen4.lua
@@ -64,11 +64,11 @@ object_draft_schematic_weapon_lightsaber_lightsaber_two_hand_gen4 = object_draft
    customizationStringNames = {},
    customizationDefaults = {},
 
-   ingredientTemplateNames = {"craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n"},
-   ingredientTitleNames = {"emitter_shroud", "primary_crystal", "activator", "handgrip", "focusing_crystals", "power_field_insulator", "energizers", "focusing_crystals", "focusing_crystals"},
-   ingredientSlotType = {0, 1, 0, 0, 1, 0, 0, 1, 1},
-   resourceTypes = {"steel_duralloy", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff", "aluminum_titanium", "petrochem_inert_polymer", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff", "gas_inert_culsion", "copper_polysteel", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff"},
-   resourceQuantities = {40, 1, 30, 46, 1, 58, 58, 1, 1},
+   ingredientTemplateNames = {"craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n"},
+   ingredientTitleNames = {"emitter_shroud", "primary_crystal", "activator", "handgrip", "1st focusing_crystal", "2nd focusing_crystal", "3rd focusing_crystal", "power_field_insulator", "energizers"},
+   ingredientSlotType = {0, 1, 0, 0, 1, 1, 1, 0, 0},
+   resourceTypes = {"steel_duralloy", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff", "aluminum_titanium", "petrochem_inert_polymer", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff", "gas_inert_culsion", "copper_polysteel"},
+   resourceQuantities = {100, 1, 100, 100, 1, 1, 1, 100, 100},
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
