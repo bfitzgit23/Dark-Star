08864594770c26ad4916057877d1aa97a450bde4
orcsbane1369 <orcsbane1369@gmail.com>
2025-07-28 14:15:43 -0500

    Update bastilla.lua

--- MMOCoreORB/bin/scripts/screenplays/custom/bastilla.lua
+++ MMOCoreORB/bin/scripts/screenplays/custom/bastilla.lua
@@ -1,32 +1,26 @@
 -- File: MMOCoreORB\bin\scripts\screenplays\custom\bastilla.lua
 
--- 1. Bastila ScreenPlay Definition
--- Define the screenplay object using a local variable (e.g., bastila_screenplay_obj)
-local bastila_screenplay_obj = ScreenPlay:new {
-	numberOfActs = 1,
-	questString = "bastilla_npc_task",
-	states = {},
+-- Define the ScreenPlay for the vendor
+dark_star_vendor = ScreenPlay:new {
+    numberOfActs = 1,
+    questString = "bastilla_task",
+    states = {},
 }
 
--- Define methods on the local screenplay object BEFORE it's registered globally
-function bastila_screenplay_obj:start()
+registerScreenPlay("bastilla", true)
+
+-- This function runs when the script starts, spawning all the vendor NPCs
+function bastilla:start()
+    -- A list of all locations where the vendor should spawn.
     local spawnLocations = {
-        { "corellia", 1000, 35, 2000, 35 },  -- Example spawn location
-        { "naboo", 2000, 35, 3000, 35 },
-        { "tatooine", 1500, 35, 2500, 35 },
+        -- Corellia
+        { "corellia", -3271, 86, 3114, 35 },      	-- cnet
     }
 
     for i, location in ipairs(spawnLocations) do
-        -- Spawn the NPC using the CREATURE TEMPLATE named "bastilla_npc"
-        -- This "bastilla_npc" creature template must be defined in bastilla_npc.lua
         spawnMobile(location[1], "bastilla_npc", 1, location[2], location[3], location[4], location[5], 0)
     end
 end
-
--- Register the local screenplay object globally *after* its methods are defined.
--- This makes the global 'bastilla_npc_quest' variable refer to 'bastila_screenplay_obj'.
-registerScreenPlay("bastilla_npc_quest", true)
-
 
 -- 2. Bastila's Conversation Handler (This part is correctly structured)
 bastilla_convo_handler = Object:new {
