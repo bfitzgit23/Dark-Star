e7f32722628ad47654d8868c97894d011eef1381
ben <dfitz@me.com>
2025-08-05 06:25:46 -0400

    Update krayt_dragon_grand.lua

--- MMOCoreORB/bin/scripts/mobile/tatooine/krayt_dragon_grand.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/krayt_dragon_grand.lua
@@ -3,17 +3,17 @@ krayt_dragon_grand = Creature:new {
 	socialGroup = "krayt",
 	faction = "",
 	mobType = MOB_CARNIVORE,
-	level = 336,
-	chanceHit = 30,
+	level = 330,
+	chanceHit = 27.0,
 	damageMin = 2270,
 	damageMax = 4250,
-	baseXp = 28549,
-	baseHAM = 410000,
-	baseHAMmax = 501000,
+	baseXp = 32549,
+	baseHAM = 510000,
+	baseHAMmax = 601000,
 	armor = 3,
-	resists = {195,195,195,195,165,195,195,195,-1},
+	resists = {185,185,185,185,165,185,185,185,125},
 	meatType = "meat_carnivore",
-	meatAmount = 1000,
+	meatAmount = 1700,
 	hideType = "hide_bristley",
 	hideAmount = 950,
 	boneType = "bone_mammal",
@@ -25,23 +25,66 @@ krayt_dragon_grand = Creature:new {
 	creatureBitmask = PACK + KILLER + STALKER,
 	optionsBitmask = AIENABLED,
 	diet = CARNIVORE,
-	scale = 2.35,
+	scale = 1.8,
 
 	templates = {"object/mobile/krayt_dragon_hue.iff"},
 	hues = { 16, 17, 18, 19, 20, 21, 22, 23 },
 
 	lootGroups = {
-		{
-			groups = {
-				{group = "krayt_tissue_rare", chance = 2000000},
-				{group = "krayt_dragon_common", chance = 2500000},
-				{group = "krayt_pearls", chance = 1500000},
-				{group = "armor_all", chance = 2000000},
-				{group = "weapons_all", chance = 2000000},
-			},
-			lootChance = 8000000
-		}
+	{
+        groups = {
+			{group = "krayt_tissue_rare", chance = 1500000},         -- 15.00% of group, 15.00% total
+			{group = "krayt_dragon_common", chance = 5000000},       -- 50.00% of group, 50.00% total
+			{group = "krayt_pearls", chance = 1500000},              -- 15.00% of group, 15.00% total
+			{group = "armor_attachments", chance = 1000000},         -- 10.00% of group, 10.00% total
+			{group = "clothing_attachments", chance = 1000000},      -- 10.00% of group, 10.00% total
+		},
+		lootChance = 10000000, -- 100.00% total chance
 	},
+	{
+        groups = {
+			{group = "krayt_tissue_rare", chance = 2500000},         -- 25.00% of group, 12.50% total
+			{group = "krayt_dragon_common", chance = 3500000},       -- 35.00% of group, 17.50% total
+			{group = "krayt_pearls", chance = 2000000},              -- 20.00% of group, 10.00% total
+			{group = "armor_attachments", chance = 1000000},         -- 10.00% of group, 5.00% total
+			{group = "clothing_attachments", chance = 1000000},      -- 10.00% of group, 5.00% total
+		},
+		lootChance = 5000000, -- 50.00% total chance
+	},
+	{
+        groups = {
+			{group = "krayt_tissue_rare", chance = 2500000},         -- 25.00% of group, 7.50% total
+			{group = "krayt_dragon_common", chance = 3500000},       -- 35.00% of group, 10.50% total
+			{group = "krayt_pearls", chance = 2000000},              -- 20.00% of group, 6.00% total
+			{group = "armor_attachments", chance = 1000000},         -- 10.00% of group, 3.00% total
+			{group = "clothing_attachments", chance = 1000000},      -- 10.00% of group, 3.00% total
+		},
+		lootChance = 3000000, -- 30.00% total chance
+	},
+	{
+        groups = {
+			{group = "krayt_tissue_rare", chance = 2500000},         -- 25.00% of group, 2.50% total
+			{group = "krayt_dragon_common", chance = 3500000},       -- 35.00% of group, 3.50% total
+			{group = "krayt_pearls", chance = 2000000},              -- 20.00% of group, 2.00% total
+			{group = "armor_attachments", chance = 1000000},         -- 10.00% of group, 1.00% total
+			{group = "clothing_attachments", chance = 1000000},      -- 10.00% of group, 1.00% total
+		},
+		lootChance = 1000000, -- 10.00% total chance
+	},
+	{
+        groups = {
+			{group = "krayt_pearls", chance = 10000000},             -- 100.00% of group, 10.00% total
+		},
+		lootChance = 1000000, -- 10.00% total chance
+	},
+	{
+        groups = {
+			{group = "krayt_pearls", chance = 10000000},             -- 100.00% of group, 5.00% total
+		},
+		lootChance = 500000, -- 5.00% total chance
+	}
+},
+
 
 	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
 	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
