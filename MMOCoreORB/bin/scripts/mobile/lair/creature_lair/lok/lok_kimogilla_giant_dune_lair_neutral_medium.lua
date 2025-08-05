3d989fab16caa31db07b08b7ec5399d6c595ee46
ben <dfitz@me.com>
2025-08-05 06:16:31 -0400

    Update lok_kimogilla_giant_dune_lair_neutral_medium.lua

--- MMOCoreORB/bin/scripts/mobile/lair/creature_lair/lok/lok_kimogilla_giant_dune_lair_neutral_medium.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_lair/lok/lok_kimogilla_giant_dune_lair_neutral_medium.lua
@@ -1,5 +1,5 @@
 lok_kimogilla_giant_dune_lair_neutral_medium = Lair:new {
-	mobiles = {{"giant_dune_kimogila",1},{"dune_kimogila",2}},
+	mobiles = {{"giant_dune_kimogila",2},{"dune_kimogila",3}},
 	spawnLimit = 7,
 	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_bones_large_fog_red.iff"},
 	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_bones_large_fog_red.iff"},
