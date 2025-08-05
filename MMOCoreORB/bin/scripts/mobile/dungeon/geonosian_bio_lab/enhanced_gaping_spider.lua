f37e23c6b52e310da5fa9339e901f0909dc95591
ben <dfitz@me.com>
2025-08-05 06:40:49 -0400

    Update enhanced_gaping_spider.lua

--- MMOCoreORB/bin/scripts/mobile/dungeon/geonosian_bio_lab/enhanced_gaping_spider.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/geonosian_bio_lab/enhanced_gaping_spider.lua
@@ -1,52 +1,53 @@
-enhanced_gaping_spider = Creature:new {
-	objectName = "@mob/creature_names:geonosian_gaping_spider_fire",
-	customName = "Fire Breathing Spider",
+acklay = Creature:new {
+	objectName = "@mob/creature_names:geonosian_acklay_bunker_boss",
+	customName = "Acklay",
 	socialGroup = "geonosian_creature",
 	mobType = MOB_CARNIVORE,
 	faction = "",
-	level = 108,
-	chanceHit = 2.5,
-	damageMin = 715,
-	damageMax = 1140,
-	baseXp = 10267,
-	baseHAM = 32000,
-	baseHAMmax = 39000,
+	level = 157,
+	chanceHit = 92.5,
+	damageMin = 935,
+	damageMax = 1580,
+	baseXp = 14884,
+	baseHAM = 96000,
+	baseHAMmax = 118000,
 	armor = 2,
-	resists = {150,10,-1,195,-1,10,10,10,-1},
-	meatType = "meat_insect",
-	meatAmount = 50,
+	resists = {130,145,155,155,145,30,30,30,-1},
+	meatType = "",
+	meatAmount = 0,
 	hideType = "",
 	hideAmount = 0,
 	boneType = "",
 	boneAmount = 0,
 	milk = 0,
 	tamingChance = 0,
-	ferocity = 0,
+	ferocity = 25,
 	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
 	creatureBitmask = PACK + KILLER,
 	optionsBitmask = AIENABLED,
 	diet = CARNIVORE,
+	tauntable = false,
 
-	templates = {"object/mobile/gaping_spider.iff"},
-	scale = 2.5,
+	templates = {"object/mobile/acklay_hue.iff"},
 	lootGroups = {
 		{
 			groups = {
-				{group = "fire_breathing_spider", chance = 10000000}
-			}
+				{group = "acklay", chance = 10000000}
+			},
+			lootChance = 10000000
 		}
 	},
 
 	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
 	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
-	primaryWeapon = "object/weapon/ranged/creature/creature_spit_heavy_flame.iff",
-	secondaryWeapon = "unarmed",
+	primaryWeapon = "unarmed",
+	secondaryWeapon = "none",
 	conversationTemplate = "",
 
 	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
 	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
-	primaryAttacks = { {"strongpoison",""}, {"stunattack",""} },
-	secondaryAttacks = { {"strongpoison",""}, {"stunattack",""} }
+	primaryAttacks = { {"posturedownattack","stateAccuracyBonus=50"}, {"creatureareacombo","stateAccuracyBonus=50"} },
+	secondaryAttacks = { }
 }
 
-CreatureTemplates:addCreatureTemplate(enhanced_gaping_spider, "enhanced_gaping_spider")
+CreatureTemplates:addCreatureTemplate(acklay, "acklay")
